d798eabcb9d2dded5f84d8650cfe73815add4ac4
Daniel Baamonde <dannyb21892@gmail.com>
2018-01-10 06:56:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var whoplayswhat = [];
-  for (let i = 0, i<musicians.length, i++) {
+  for (let i = 0, i < musicians.length, i++) {
     whoplayswhat[i] = `${musicians[i]} plays ${instruments[i]}`
   }
   return whoplayswhat
